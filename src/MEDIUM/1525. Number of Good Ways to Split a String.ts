da9000a9130cba643dcf1a6f340f372925f9c6b3
Ziqian Cheng <ziqian.cheng@shopee.com>
2020-12-31 17:21:37 +0800

    12/31/2020

---
+++ src/MEDIUM/1525. Number of Good Ways to Split a String.ts
@@ -0,0 +1,69 @@
+// 12/31/2020 MEDIUM
+
+// https://leetcode-cn.com/problems/number-of-good-ways-to-split-a-string/
+
+/*
+You are given a string s, a split is called good if you can split s into 2 non-empty strings p and q 
+where its concatenation is equal to s and the number of distinct letters in p and q are the same.
+
+Return the number of good splits you can make in s.
+
+ 
+
+Example 1:
+
+Input: s = "aacaba"
+Output: 2
+Explanation: There are 5 ways to split "aacaba" and 2 of them are good. 
+("a", "acaba") Left string and right string contains 1 and 3 different letters respectively.
+("aa", "caba") Left string and right string contains 1 and 3 different letters respectively.
+("aac", "aba") Left string and right string contains 2 and 2 different letters respectively (good split).
+("aaca", "ba") Left string and right string contains 2 and 2 different letters respectively (good split).
+("aacab", "a") Left string and right string contains 3 and 1 different letters respectively.
+Example 2:
+
+Input: s = "abcd"
+Output: 1
+Explanation: Split the string as follows ("ab", "cd").
+Example 3:
+
+Input: s = "aaaaa"
+Output: 4
+Explanation: All possible splits are good.
+Example 4:
+
+Input: s = "acbadbaada"
+Output: 2
+ 
+
+Constraints:
+
+s contains only lowercase English letters.
+1 <= s.length <= 10^5
+
+ */
+
+function numSplits(s: string): number {
+  const charCountRight = new Array(26).fill(0);
+  const charCountLeft = new Array(26).fill(0);
+  let rightUnionCount = 0, leftUnionCount = 0;
+  for (let i = 0; i < s.length; i++) {
+    const index = getCharCode(s[i]);
+    if (charCountRight[index] === 0) rightUnionCount++;
+    charCountRight[index]++;
+  }
+  if (rightUnionCount === 1) return s.length - 1;
+  let ans = 0;
+  for (let i = 0; i < s.length - 1; i++) {
+    const index = getCharCode(s[i]);
+    charCountRight[index]--;
+    if (charCountRight[index] === 0) rightUnionCount--;
+    if (charCountLeft[index] === 0) leftUnionCount++;
+    charCountLeft[index]++;
+    if (leftUnionCount === rightUnionCount) ans++;
+  }
+  return ans;
+  function getCharCode(char: string) {
+    return char.charCodeAt(0) - 'a'.charCodeAt(0);
+  }
+};
